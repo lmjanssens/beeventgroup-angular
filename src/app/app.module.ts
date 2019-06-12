c493e3c07154e5beedafe56d504e604ff4be1061
Robin Silvério <robinsilverio17@gmail.com>
2019-06-12 11:23:32 +0200

    Temporarily commented import lines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,8 +41,8 @@ import {ApiService} from "./services/api.service";
 import {AuthGuard} from "./services/auth.guard.service";
 import {AuthorizationService} from "./services/authorization.service";
 import { InstructorCreateComponent } from './components/instructor/instructor-create/instructor-create.component';
-import { DetailsComponent } from './components/customer/customer/details/details.component';
-import { CustomerDetailsComponent } from './components/customer/customer-details/customer-details.component';
+// import { DetailsComponent } from './components/customer/customer/details/details.component';
+// import { CustomerDetailsComponent } from './components/customer/customer-details/customer-details.component';
 
 @NgModule({
   declarations: [
@@ -70,9 +70,9 @@ import { CustomerDetailsComponent } from './components/customer/customer-details
     CateringOverviewComponent,
     EventmanagerAgendaComponent,
     CustomerUpdateComponent,
-    InstructorCreateComponent,
-    DetailsComponent,
-    CustomerDetailsComponent
+    InstructorCreateComponent
+    // DetailsComponent,
+    // CustomerDetailsComponent
   ],
   imports: [
     BrowserModule,
