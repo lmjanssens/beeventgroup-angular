4d09052f68e34d0c515d38525c33e6e566d302b9
bashar <dhr.b.farah@oulook.com>
2019-06-17 14:11:56 +0200

    "pdf generator basic"

--- src/app/components/pdf-generator/pdf-generator.component.ts
+++ src/app/components/pdf-generator/pdf-generator.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Globals} from '../globals';
+import * as jsPDF from 'jspdf';
+
 
 @Component({
   selector: 'app-pdf-generator',
@@ -7,11 +9,30 @@ import {Globals} from '../globals';
   styleUrls: ['./pdf-generator.component.css']
 })
 export class PdfGeneratorComponent implements OnInit {
+  @ViewChild('content') content: ElementRef;
 
-  constructor(private globals: Globals) { }
+  constructor(private globals: Globals) {
+  }
+
 
   ngOnInit() {
     this.globals.setHuidigePagina('htmlFormulier');
   }
 
+  downloadPDF() {
+    const doc = new jsPDF();
+    let specialElementHandlers = {
+      '#editor': function (elemnt, renderer) {
+        return true;
+      }
+    };
+    let content = this.content.nativeElement;
+
+    doc.fromHTML(content.innerHTML, 15, 15, {
+      'width': 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('test.pdf');
+  }
 }
