cddf5dc8b26bcdb9ba2c5f714cf7b82d8ce2b08d
Mark Bueno <markbuenoguzman@hotmail.com>
2019-06-19 15:14:16 +0200

    Agenda werkend met events

--- src/app/components/eventmanager-agenda/eventmanager-agenda.component.ts
+++ src/app/components/eventmanager-agenda/eventmanager-agenda.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import {Globals} from '../globals';
 import {NavbarComponent} from '../../navbar/navbar.component';
 import {Order} from '../../models/order.model';
+import {ReservationService} from '../../services/reservation.service';
+import {FullCalendarComponent} from '@fullcalendar/angular';
 
 
 @Component({
@@ -13,38 +15,44 @@ import {Order} from '../../models/order.model';
 export class EventmanagerAgendaComponent implements OnInit {
   i = 0;
   calendarPlugins = [dayGridPlugin];
-  public calenderEvents = [];
-  public orderList: Order[] = [
-    // new Order(1, null, '1-1-1', '2019-07-01',
-    //   '', '', '', null, null, null, null, null),
-    // new Order(2, null, '1-1-1', '2019-07-02',
-    //   '', '', '', null, null, null, null, null),
-    // new Order(3, null, '1-1-1', '2019-07-03',
-    //   '', '', '', null, null, null, null, null),
-    // new Order(4, null, '1-1-1', '2019-07-04',
-    //   '', '', '', null, null, null, null, null),
-    // new Order(5, null, '1-1-1', '2019-07-05',
-    //   '', '', '', null, null, null, null, null),
-    // new Order(6, null, '1-1-1', '2019-07-06',
-    //   '', '', '', null, null, null, null, null)
-  ];
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
 
-  constructor(private globals: Globals, private navbar: NavbarComponent) {
+  public orderList: Order[] = [
+    new Order(1, null, '1-1-1', '2019-07-01',
+      '', '', '', null, null, null),
+    new Order(2, null, '1-1-1', '2019-07-02',
+      '', '', '', null, null, null),
+    new Order(3, null, '1-1-1', '2019-07-03',
+      '', '', '', null, null, null),
+    new Order(4, null, '1-1-1', '2019-07-04',
+      '', '', '', null, null, null),
+    new Order(5, null, '1-1-1', '2019-07-05',
+      '', '', '', null, null, null),
+    new Order(6, null, '1-1-1', '2019-07-06',
+      '', '', '', null, null, null)];
+
+  constructor(private globals: Globals, private navbar: NavbarComponent, private reservationService: ReservationService) {
   }
 
-  fillCalenderEvents() {
-    while (this.i < this.orderList.length) {
-      this.calenderEvents.push({title: this.orderList[this.i].orderId.toString(), date: this.orderList[this.i].dateevent});
-      console.log(this.calenderEvents);
+  fillCalenderEvents(list) {
+    console.log(list);
+    while (this.i < list.length) {
+      this.calendarComponent.getApi().addEvent({title: list[this.i].event.name, start: list[this.i].dateevent});
       this.i = this.i + 1;
     }
+
+    return list;
+  }
+
+  getOrders() {
+    this.reservationService.getAll().subscribe(data => {
+      this.fillCalenderEvents(data);
+    });
   }
 
   ngOnInit() {
     this.globals.setHuidigePagina('Agenda');
     this.navbar.checkNavBarStyle();
-    console.log(this.globals.getHuidigePagina());
-    this.fillCalenderEvents();
+    this.getOrders();
   }
-
 }
