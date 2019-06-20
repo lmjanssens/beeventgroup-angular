f6a1c7febed0e321378047d002a5da5fdeb91719
joppa0808 <de_ballen_man@hotmail.com>
2019-06-21 01:21:37 +0200

    create contract fix

--- src/app/components/supplier/supplier-contract-create/supplier-contract-create.component.ts
+++ src/app/components/supplier/supplier-contract-create/supplier-contract-create.component.ts
@@ -12,14 +12,13 @@ import {SupplierContract} from '../../../models/supplier-contract.model';
   styleUrls: ['./supplier-contract-create.component.css']
 })
 export class SupplierContractCreateComponent implements OnInit {
-
   supplier: Supplier = new Supplier();
-  contracts: SupplierContract[] = [];
   contract: SupplierContract = new SupplierContract();
   loading: true;
   currentId;
   currentContractId;
   private sub: any;
+  newContract: any;
 
   constructor(private globals: Globals, private supplierService: SupplierService, private route: ActivatedRoute, private router: Router) {
   }
@@ -31,17 +30,25 @@ export class SupplierContractCreateComponent implements OnInit {
       console.log(this.currentId + this.currentContractId);
       this.supplierService.getById(this.currentId).subscribe(supplier => {
         this.supplier = supplier;
-        this.contracts = this.supplier.contracts;
-        for (let a of this.contracts) {
-          if (a.id.toString() === this.currentContractId) {
-            this.contract = a;
-          }
-        }
-
       });
     });
   }
   ngSubmit(f: NgForm) {
+    this.newContract = new SupplierContract();
+    this.newContract.supplierid = this.supplier.supplierid
+    this.newContract.type = this.contract.type;
+    this.newContract.title = this.contract.title;
+    this.newContract.description = this.contract.description;
+    this.newContract.inclubtw = this.contract.inclubtw;
+    this.newContract.exclubtw = this.contract.exclubtw;
+    this.newContract.btw_percentage = this.contract.btw_percentage;
+    this.newContract.pre_conditions = this.contract.pre_conditions;
+    this.newContract.insurance = this.contract.insurance;
+    this.newContract.responsibility = this.contract.responsibility;
+    this.newContract.extras = this.contract.extras;
+    this.newContract.startDate = this.contract.startDate;
+    this.newContract.endDate = this.contract.endDate;
+    this.supplier.contracts.push(this.newContract);
     const data = JSON.parse(JSON.stringify(this.supplier)) as any;
     console.log(data);
     this.supplierService.updateSupplier(data).subscribe(() => {
