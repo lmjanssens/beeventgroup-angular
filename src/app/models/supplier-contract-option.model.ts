041988421405c3aa2d1be6bc7a75cab095a12ea7
Mark Bueno <markbuenoguzman@hotmail.com>
2019-06-21 16:55:57 +0200

    MOdel Push

---
+++ src/app/models/supplier-contract-option.model.ts
@@ -0,0 +1,7 @@
+import {SupplierContract} from './supplier-contract.model';
+
+export class SupplierContractOption {
+  public optionid: number;
+  public contract: SupplierContract;
+  public option: string;
+}
